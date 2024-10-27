03e1afa184f1cef06d893d811bba82575b370767
Benjtkim <ben.jt.kim@gmail.com>
2024-10-27 10:55:01 -0400

    Commit 1 10/27/24

--- Problem Set 5/StringRecursion.java
+++ Problem Set 5/StringRecursion.java
@@ -28,11 +28,24 @@ public class StringRecursion {
     public static int find(char ch, String str) {
         if (str == null || str.equals("")) {
             return -1;
+        } else if (ch == str.charAt(0)) {
+          return 0;
         }
-        int rest = find(ch, str.substring(1));
-        if (str.charAt(0) == ch) {
-            rest = str.length() - 1;
-        }
+        int rest = 1 + find(ch, str.substring(1));
         return rest;
+    }
+
+    public static String weave(String str1, String str2) {
+        if (str1 == null || str2 == null) {
+            throw new IllegalArgumentException();
+        } else if (str1.equals("") && !str2.equals("")) {
+            return "" + str2;
+        } else if (str2.equals("") && !str1.equals("")) {
+            return "" + str1;
+        } else if (str1.equals("") && str2.equals("")) {
+            return "";
+        }
+        String rest = weave(str1.substring(1), str2.substring(1));
+        return str1.charAt(0) + "" + str2.charAt(0) + rest;
     }
 }
