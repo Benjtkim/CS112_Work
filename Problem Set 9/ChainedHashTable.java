996c2ebe29ee4146a9c42c6ec806cbb8fb76fbc4
Benjtkim <ben.jt.kim@gmail.com>
2024-12-10 14:56:46 -0500

    PS8 Final Commit(?) 12/10/24

--- Problem Set 9/ChainedHashTable.java
+++ Problem Set 9/ChainedHashTable.java
@@ -1,11 +1,14 @@
 /*
  * ChainedHashTable.java
+ * Description: Class for a hash table that uses separate chaining.
  *
  * Computer Science 112, Boston University
  * 
  * Modifications and additions by:
- *     name:
- *     email:
+ *     name: Benjamin Kim
+ *     email: benjt@bu.edu
+ * 
+ * Date: 12/9/24
  */
 
 import java.util.*;     // to allow for the use of Arrays.toString() in testing
@@ -44,6 +47,13 @@ public class ChainedHashTable implements HashTable {
     }
     
     /*** Add your constructor here ***/
+    public ChainedHashTable(int size) {
+        if (size < 0) {
+            throw new IllegalArgumentException();
+        }
+        this.table = new Node[size];
+        this.numKeys = 0;
+    }
     
     
     /*
@@ -52,7 +62,29 @@ public class ChainedHashTable implements HashTable {
      */
     public boolean insert(Object key, Object value) {
         /** Replace the following line with your implementation. **/
-        return false;
+        int index = h1(key);
+        if (this.table[index] == null) {
+            this.table[index] = new Node(key, value);
+        } else {
+            boolean alreadyExists = false;
+            Node head = this.table[index];
+            Node trav = head;
+            Node newNode = new Node(key, value);
+            while (trav != null) {
+                if (trav.key == key) {
+                    alreadyExists = true;
+                }
+                trav = trav.next;
+            }
+            if (alreadyExists == false) {
+                newNode.next = head;
+                this.table[index] = newNode;
+            } else {
+                return false;
+            }
+        }
+        this.numKeys += 1;
+        return true;
     }
     
     /*
@@ -60,8 +92,19 @@ public class ChainedHashTable implements HashTable {
      * associated collection of values, or null if the key 
      * is not in the table
      */
-    public Queue<Object> search(Object key) {
+    public LLQueue<Object> search(Object key) {
         /** Replace the following line with your implementation. **/
+        int index = h1(key);
+        if (this.table[index] == null) {
+            return null;
+        }
+        Node trav = this.table[index];
+        while (trav != null) {
+            if (trav.key.equals(key)) {
+                return trav.values;
+            }
+            trav = trav.next;
+        }
         return null;
     }
     
@@ -70,15 +113,91 @@ public class ChainedHashTable implements HashTable {
      * and return the associated collection of values, or null if the key 
      * is not in the table
      */
-    public Queue<Object> remove(Object key) {
+    public LLQueue<Object> remove(Object key) {
         /** Replace the following line with your implementation. **/
-        return null;
+        int index = h1(key);
+        if (this.table[index] == null) {
+            return null;
+        }
+        Node trail = null;
+        Node trav = this.table[index];
+        while (trav.key != key) {
+            trail = trav;
+            trav = trav.next;
+        }
+        if (trail == null) {
+            LLQueue<Object> toReturn = this.table[index].values;
+            this.table[index] = this.table[index].next;
+            this.numKeys -= 1;
+            return toReturn;
+        } else {
+            LLQueue<Object> toReturn = trav.values;
+            trail.next = null;
+            this.numKeys -= 1;
+            return toReturn;
+        }
     }
     
     
     /*** Add the other required methods here ***/
+    public int getNumKeys() {
+        return this.numKeys;
+    }
+
+    public double load() {
+        double numKeys = 0;
+        for (int i = 0; i < this.table.length; i++) {
+            if (this.table[i] == null) {
+                continue;
+            }
+            Node trav = this.table[i];
+            while (trav != null) {
+                numKeys++;
+                trav = trav.next;
+            }
+        }
+        return numKeys / this.table.length;
+    }
     
-    
+    public Object[] getAllKeys() {
+        Object[] result = new Object[getNumKeys()];
+        int currPosition = 0;
+        for (int i = 0; i < this.table.length; i++) {
+            if (this.table[i] == null) {
+                continue;
+            }
+            Node trav = this.table[i];
+            while (trav != null) {
+                result[currPosition] = trav.key;
+                currPosition++;
+                trav = trav.next;
+            }
+        }
+        return result;
+    }
+
+    public void resize(int newSize) {
+        Object[] allKeys = getAllKeys();
+        Object[] allValues = new Object[allKeys.length];
+        int currPosition = 0;
+        for (int i = 0; i < this.table.length; i++) {
+            if (this.table[i] == null) {
+                continue;
+            }
+            Node trav = this.table[i];
+            while (trav != null) {
+                allValues[currPosition] = trav.values.remove();
+                trav = trav.next;
+            }
+        }
+        this.table = new Node[newSize];
+        this.numKeys = 0;
+        for (int i = 0; i < allKeys.length; i++) {
+            insert(allKeys[i], allValues[i]);
+        }
+    }
+
+
     
     /*
      * toString - returns a string representation of this ChainedHashTable
@@ -114,6 +233,288 @@ public class ChainedHashTable implements HashTable {
     }
 
     public static void main(String[] args) {
+
         /** Add your unit tests here **/
+        System.out.println("(1) insert test 1");
+        try {
+            ChainedHashTable table = new ChainedHashTable(5);
+            
+            boolean results = table.insert("howdy", 15);
+            System.out.println("actual results:");
+            System.out.println(results);
+            System.out.println("expected results:");
+            System.out.println(true);
+            System.out.print("MATCHES EXPECTED RESULTS?: ");
+            System.out.println(results == true);
+        } catch (Exception e) {
+            System.out.println("INCORRECTLY THREW AN EXCEPTION: " + e);
+        }
+                           
+        System.out.println();   
+
+        System.out.println("(2) insert test 2");
+        try {
+            ChainedHashTable table = new ChainedHashTable(5);
+            table.insert("howdy", 15);
+
+            boolean results = table.insert("howdy", 25);
+            System.out.println("actual results:");
+            System.out.println(results);
+            System.out.println("expected results:");
+            System.out.println(false);
+            System.out.print("MATCHES EXPECTED RESULTS?: ");
+            System.out.println(results == false);
+        } catch (Exception e) {
+            System.out.println("INCORRECTLY THREW AN EXCEPTION: " + e);
+        }
+                           
+        System.out.println();  
+
+        System.out.println("(3) Search test 1");
+        try {
+            ChainedHashTable table = new ChainedHashTable(5);
+            table.insert("howdy", 15);
+
+            LLQueue<Object> results = table.search("howdy");
+
+            System.out.println("actual results:");
+            System.out.println(results);
+            System.out.println("expected results:");
+            LLQueue<Object> expectedResults = new LLQueue<Object>();
+            expectedResults.insert(15);
+            System.out.println(expectedResults);
+            System.out.print("MATCHES EXPECTED RESULTS?: ");
+            System.out.println(results.peek() == expectedResults.peek());
+        } catch (Exception e) {
+            System.out.println("INCORRECTLY THREW AN EXCEPTION: " + e);
+        }
+                           
+        System.out.println();  
+
+        System.out.println("(4) Search test 2");
+        try {
+            ChainedHashTable table = new ChainedHashTable(5);
+            table.insert("apple", 5);
+
+            LLQueue<Object> results = table.search("goodbye");
+
+            System.out.println("actual results:");
+            System.out.println(results);
+            System.out.println("expected results:");
+            System.out.println("null");
+            System.out.print("MATCHES EXPECTED RESULTS?: ");
+            System.out.println(results == null);
+        } catch (Exception e) {
+            System.out.println("INCORRECTLY THREW AN EXCEPTION: " + e);
+        }
+                           
+        System.out.println();  
+
+        System.out.println("(5) remove test 1");
+        try {
+            ChainedHashTable table = new ChainedHashTable(5);
+            table.insert("apple", 5);
+
+            LLQueue<Object> results = table.remove("goodbye");
+
+            System.out.println("actual results:");
+            System.out.println(results);
+            System.out.println("expected results:");
+            System.out.println("null");
+            System.out.print("MATCHES EXPECTED RESULTS?: ");
+            System.out.println(results == null);
+        } catch (Exception e) {
+            System.out.println("INCORRECTLY THREW AN EXCEPTION: " + e);
+        }
+                           
+        System.out.println();  
+
+        System.out.println("(6) remove test 2");
+        try {
+            ChainedHashTable table = new ChainedHashTable(5);
+            table.insert("apple", 5);
+
+            LLQueue<Object> results = table.remove("apple");
+
+            System.out.println("actual results:");
+            System.out.println(results);
+            System.out.println("expected results:");
+            LLQueue<Object> expectedResults = new LLQueue<Object>();
+            expectedResults.insert(5);
+            System.out.println(expectedResults);
+            System.out.print("MATCHES EXPECTED RESULTS?: ");
+            System.out.println(results.peek() == expectedResults.peek());
+        } catch (Exception e) {
+            System.out.println("INCORRECTLY THREW AN EXCEPTION: " + e);
+        }
+                           
+        System.out.println();  
+
+        System.out.println("(7) getNumKeys test 1");
+        try {
+            ChainedHashTable table = new ChainedHashTable(5);
+
+            int results = table.getNumKeys();
+
+            System.out.println("actual results:");
+            System.out.println(results);
+            System.out.println("expected results:");
+            System.out.println(0);
+            System.out.print("MATCHES EXPECTED RESULTS?: ");
+            System.out.println(results == 0);
+        } catch (Exception e) {
+            System.out.println("INCORRECTLY THREW AN EXCEPTION: " + e);
+        }
+                           
+        System.out.println();  
+
+        System.out.println("(8) getNumKeys test 2");
+        try {
+            ChainedHashTable table = new ChainedHashTable(5);
+            table.insert("apple", 5);
+            table.insert("howdy", 15);
+            table.insert("howdy", 25);
+            table.insert("crimson", 10);
+            table.insert("table", 60);
+
+            int results = table.getNumKeys();
+
+            System.out.println("actual results:");
+            System.out.println(results);
+            System.out.println("expected results:");
+            System.out.println(4);
+            System.out.print("MATCHES EXPECTED RESULTS?: ");
+            System.out.println(results == 4);
+        } catch (Exception e) {
+            System.out.println("INCORRECTLY THREW AN EXCEPTION: " + e);
+        }
+                           
+        System.out.println();  
+
+        System.out.println("(9) load test 1");
+        try {
+            ChainedHashTable table = new ChainedHashTable(5);
+
+            double results = table.load();
+
+            System.out.println("actual results:");
+            System.out.println(results);
+            System.out.println("expected results:");
+            System.out.println(0.0);
+            System.out.print("MATCHES EXPECTED RESULTS?: ");
+            System.out.println(results == 0.0);
+        } catch (Exception e) {
+            System.out.println("INCORRECTLY THREW AN EXCEPTION: " + e);
+        }
+                           
+        System.out.println();  
+
+        System.out.println("(10) load test 2");
+        try {
+            ChainedHashTable table = new ChainedHashTable(5);
+            table.insert("howdy", 15);
+            table.insert("goodbye", 10);
+            table.insert("apple", 5);
+
+            double results = table.load();
+
+            System.out.println("actual results:");
+            System.out.println(results);
+            System.out.println("expected results:");
+            System.out.println(0.6);
+            System.out.print("MATCHES EXPECTED RESULTS?: ");
+            System.out.println(results == 0.6);
+        } catch (Exception e) {
+            System.out.println("INCORRECTLY THREW AN EXCEPTION: " + e);
+        }
+                           
+        System.out.println();  
+
+        System.out.println("(11) getAllKeys test 1");
+        try {
+            ChainedHashTable table = new ChainedHashTable(5);
+
+            Object[] results = table.getAllKeys();
+
+            System.out.println("actual results:");
+            System.out.println(Arrays.toString(results));
+            System.out.println("expected results:");
+            Object[] expectedResults = new Object[0];
+            System.out.println(Arrays.toString(expectedResults));
+            System.out.print("MATCHES EXPECTED RESULTS?: ");
+            System.out.println(Arrays.toString(results).equals(Arrays.toString(expectedResults)));
+        } catch (Exception e) {
+            System.out.println("INCORRECTLY THREW AN EXCEPTION: " + e);
+        }
+                           
+        System.out.println();  
+
+        System.out.println("(12) getAllKeys test 2");
+        try {
+            ChainedHashTable table = new ChainedHashTable(5);
+            table.insert("howdy", 15);
+            table.insert("goodbye", 10);
+            table.insert("apple", 5);
+            table.insert("howdy", 25);    
+
+            Object[] results = table.getAllKeys();
+
+            System.out.println("actual results:");
+            System.out.println(Arrays.toString(results));
+            System.out.println("expected results:");
+            Object[] expectedResults = {"apple", "howdy", "goodbye"};
+            System.out.println(Arrays.toString(expectedResults));
+            System.out.print("MATCHES EXPECTED RESULTS?: ");
+            System.out.println(Arrays.toString(results).equals(Arrays.toString(expectedResults)));
+        } catch (Exception e) {
+            System.out.println("INCORRECTLY THREW AN EXCEPTION: " + e);
+        }
+                           
+        System.out.println(); 
+        
+        System.out.println("(13) resize test 1");
+        try {
+            ChainedHashTable table = new ChainedHashTable(5); 
+
+            table.resize(7);
+
+            String results = table.toString();
+            System.out.println("actual results:");
+            System.out.println(results);
+            System.out.println("expected results:");
+            Object[] expectedResults = new Object[7];
+            System.out.println(Arrays.toString(expectedResults));
+            System.out.print("MATCHES EXPECTED RESULTS?: ");
+            System.out.println(results.equals(Arrays.toString(expectedResults)));
+        } catch (Exception e) {
+            System.out.println("INCORRECTLY THREW AN EXCEPTION: " + e);
+        }
+                           
+        System.out.println();  
+
+        System.out.println("(14) resize test 2");
+        try {
+            ChainedHashTable table = new ChainedHashTable(5);
+            table.insert("howdy", 15);
+            table.insert("goodbye", 10);
+            table.insert("apple", 5);
+            table.insert("goodbye", 20);
+
+            table.resize(7);
+
+            String results = table.toString();
+            System.out.println("actual results:");
+            System.out.println(results);
+            System.out.println("expected results:");
+            Object[] expectedResults = new Object[7];
+            expectedResults[1] = "{apple}";
+            expectedResults[5] = "{howdy}";
+            expectedResults[6] = "{goodbye}";
+            System.out.println(Arrays.toString(expectedResults));
+            System.out.print("MATCHES EXPECTED RESULTS?: ");
+            System.out.println(results.equals(Arrays.toString(expectedResults)));
+        } catch (Exception e) {
+            System.out.println("INCORRECTLY THREW AN EXCEPTION: " + e);
+        }
     }
 }
--- Problem Set 9/LinkedTree.java
+++ Problem Set 9/LinkedTree.java
@@ -1,11 +1,14 @@
 /*
  * LinkedTree.java
+ * Description: LinkedTree class for binary search trees.
  *
  * Computer Science 112
  *
  * Modifications and additions by:
- *     name:
- *     username:
+ *     name: Benjamin Kim
+ *     email: benjt@bu.edu
+ * 
+ * Date: 12/9/24
  */
 
 import java.util.*;
@@ -420,63 +423,63 @@ public class LinkedTree {
     }
     
     /* Returns a preorder iterator for this tree. */
-    public LinkedTreeIterator preorderIterator() {
-        return new PreorderIterator();
-    }
+    // public LinkedTreeIterator preorderIterator() {
+    //     return new PreorderIterator();
+    // }
     
-    /* 
-     * inner class for a preorder iterator 
-     * IMPORTANT: You will not be able to actually use objects from this class
-     * to perform a preorder iteration until you have modified the LinkedTree
-     * methods so that they maintain the parent fields in the Nodes.
-     */
-    private class PreorderIterator implements LinkedTreeIterator {
-        private Node nextNode;
+    // /* 
+    //  * inner class for a preorder iterator 
+    //  * IMPORTANT: You will not be able to actually use objects from this class
+    //  * to perform a preorder iteration until you have modified the LinkedTree
+    //  * methods so that they maintain the parent fields in the Nodes.
+    //  */
+    // private class PreorderIterator implements LinkedTreeIterator {
+    //     private Node nextNode;
         
-        private PreorderIterator() {
-            // The traversal starts with the root node.
-            nextNode = root;
-        }
+    //     private PreorderIterator() {
+    //         // The traversal starts with the root node.
+    //         nextNode = root;
+    //     }
         
-        public boolean hasNext() {
-            return (nextNode != null);
-        }
+    //     public boolean hasNext() {
+    //         return (nextNode != null);
+    //     }
         
-        public int next() {
-            if (nextNode == null) {
-                throw new NoSuchElementException();
-            }
+    //     public int next() {
+    //         if (nextNode == null) {
+    //             throw new NoSuchElementException();
+    //         }
             
-            // Store a copy of the key to be returned.
-            int key = nextNode.key;
+    //         // Store a copy of the key to be returned.
+    //         int key = nextNode.key;
             
-            // Advance nextNode.
-            if (nextNode.left != null) {
-                nextNode = nextNode.left;
-            } else if (nextNode.right != null) {
-                nextNode = nextNode.right;
-            } else {
-                // We've just visited a leaf node.
-                // Go back up the tree until we find a node
-                // with a right child that we haven't seen yet.
-                Node parent = nextNode.parent;
-                Node child = nextNode;
-                while (parent != null &&
-                       (parent.right == child || parent.right == null)) {
-                    child = parent;
-                    parent = parent.parent;
-                }
+    //         // Advance nextNode.
+    //         if (nextNode.left != null) {
+    //             nextNode = nextNode.left;
+    //         } else if (nextNode.right != null) {
+    //             nextNode = nextNode.right;
+    //         } else {
+    //             // We've just visited a leaf node.
+    //             // Go back up the tree until we find a node
+    //             // with a right child that we haven't seen yet.
+    //             Node parent = nextNode.parent;
+    //             Node child = nextNode;
+    //             while (parent != null &&
+    //                    (parent.right == child || parent.right == null)) {
+    //                 child = parent;
+    //                 parent = parent.parent;
+    //             }
                 
-                if (parent == null) {
-                    nextNode = null;  // the traversal is complete
-                } else {
-                    nextNode = parent.right;
-                }
-            }
+    //             if (parent == null) {
+    //                 nextNode = null;  // the traversal is complete
+    //             } else {
+    //                 nextNode = parent.right;
+    //             }
+    //         }
             
-            return key;
-        }
-    }
+    //         return key;
+    //     }
+    // }
     
     /*
      * "wrapper method" for the recursive depthInTree() method
@@ -518,8 +521,6 @@ public class LinkedTree {
     }
     
     public static void main(String[] args) {
-        System.out.println("--- Testing depth()/depthInTree() from Problem 4 ---");
-        System.out.println();
         System.out.println("(0) Testing on tree from Problem 3, depth of 28 node");
         try {
             LinkedTree tree = new LinkedTree();
